1cd8f4610c912cadc35fd7480b03c56a5f64097e
Jason <jason0853@gmail.com>
2017-05-04 20:10:32 +0900

    Add semantic-ui

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import reactIcon from '../../img/react.png';
+import { Button } from 'semantic-ui-react';
 
 class App extends Component {
     render() {
         return (
             <div>
                 Hello World!!
-                <img src={reactIcon} alt=""/>
+                <Button>
+                    Click Here
+                </Button>
             </div>
         );
     }
