ac46d25ccabc2f386254d6cee15071ab306d04c6
Jason <jason0853@gmail.com>
2017-05-08 17:50:48 +0900

    logo markup

--- client/src/js/components/intro/Intro.js
+++ client/src/js/components/intro/Intro.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import introImage from '../../../img/intro_bg.jpg';
 
 const Intro = () => {
     return (
         <div className="intro" style={{ backgroundImage: `url(${introImage})` }}>
-            <h1>Let's play with english!</h1>
+            <h1>
+                <Link to="/">
+                    Public English
+                </Link>
+            </h1>
         </div>
     );
 };
