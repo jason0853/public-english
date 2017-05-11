821e29f01f17eef20daf2b7d3fb7a9d36e710db7
Jason <jason0853@gmail.com>
2017-05-12 02:46:09 +0900

    Create a validateInput file

--- server/api/user/userController.js
+++ server/api/user/userController.js
@@ -1,48 +1,13 @@
-const Validator = require('validator');
-const isEmpty = require('lodash/isEmpty');
-
-function validateInput(data) {
-    let errors = {};
-
-    if (Validator.isEmpty(data.username)) {
-        errors.username = 'Username is required';
-    }
-
-    if (Validator.isEmpty(data.email)) {
-        errors.email = 'Email is required';
-    } else if (!Validator.isEmail(data.email)) {
-        errors.email = 'Email is invalid';
-    }
-
-    if (Validator.isEmpty(data.password)) {
-        errors.password = 'Password is required';
-    }
-
-    if (Validator.isEmpty(data.passwordConfirm)) {
-        errors.passwordConfirm = 'Password confirm is required';
-    }
-
-    if (!Validator.equals(data.password, data.passwordConfirm)) {
-        errors.passwordConfirm = 'Passwords must match!';
-    }
-
-    if (Validator.isEmpty(data.language)) {
-        errors.language = 'Language is required';
-    }
-   
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    }
-}
+const validateInput = require('../../helpers/validateInput');
 
 exports.register = (req, res) => {
-    const { errors, isValid } = validateInput(req.body);
+    setTimeout(() => {
+        const { errors, isValid } = validateInput(req.body);
 
-    if (!isValid) {
-        res.status(400).json(errors);
-    } else {
-        res.status(200).json({success: true});
-    }
-    
+        if (!isValid) {
+            res.status(400).json(errors);
+        } else {
+            res.status(200).json({success: true});
+        }
+    }, 1000);
 }
