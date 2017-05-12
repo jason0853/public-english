18543aef638e50bc4ca82fff4ddf2d7a8a4b7902
Jason <jason0853@gmail.com>
2017-05-12 21:03:27 +0900

    close button on message

--- client/src/js/components/intro/Intro.js
+++ client/src/js/components/intro/Intro.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { List } from 'immutable';
 import { Message } from 'semantic-ui-react';
@@ -9,28 +9,50 @@ import Greet from './Greet';
 
 import introBg from '../../../img/intro_bg.jpg';
 
-const Intro = ({message}) => {
-    const messageList = message.map(
-        (msg) => (
-            <Message 
-                key={msg.get('id')} 
-                content={msg.get('text')}
-                className="success-message"
-                color="green"
-            />
-        )
-    );
-    return (
-        <div className="intro" style={{ backgroundImage: `url(${introBg})` }}>
-            <div className="top">
-                <Logo />
-                <Buttons />
+class Intro extends Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            hidden: false
+        }
+
+        this.handleDismiss = this.handleDismiss.bind(this);
+    }
+
+    handleDismiss() {
+        this.setState({
+            hidden: !this.state.hidden
+        });
+    }
+
+    render() {
+        const { message } = this.props;
+        const messageList = message.map(
+            (msg) => (
+                <Message 
+                    key={msg.get('id')} 
+                    content={msg.get('text')}
+                    className="success-message"
+                    color="green"
+                    onDismiss={this.handleDismiss}
+                    hidden={this.state.hidden}
+                />
+            )
+        );
+        return (
+            <div className="intro" style={{ backgroundImage: `url(${introBg})` }}>
+                <div className="top">
+                    <Logo />
+                    <Buttons />
+                </div>
+                {messageList}
+                <Greet />
             </div>
-            {messageList}
-            <Greet />
-        </div>
-    );
-};
+        );
+    }
+}
 
 Intro.propTypes = {
     message: PropTypes.instanceOf(List)
