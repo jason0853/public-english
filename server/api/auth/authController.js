4c94fb16112ca998eacf2c93243870efba676941
Jason <jason0853@gmail.com>
2017-05-17 22:24:53 +0900

    Create a token

--- server/api/auth/authController.js
+++ server/api/auth/authController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../../models/user');
+const config = require('../../config');
 
 exports.login = (req, res) => {
     const { identifier, password } = req.body;
@@ -8,8 +9,13 @@ exports.login = (req, res) => {
     User.findUser(identifier)
     .then((user) => {
         if (bcrypt.compareSync(password, user[0].get('password_digest'))) {
-            const token = jwt.sign({foo: 'bar'}, 'shhhhhh');
-            console.log(token);
+            console.log(user[0])
+            const token = jwt.sign({
+                id: user[0].get('_id'),
+                username: user[0].get('username')
+            }, config.jwtSecret);
+            
+            res.json({ token });
         
         } else {
             res.status(401).json({errors: {form: 'Invalid Credentials'}});
